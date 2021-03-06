992ba85e59e078be646648357bda5f59181ebd74
Fernando Maia <fermaiasoares@aol.com>
2021-03-06 14:27:01 -0300

    Padronizacao do Codigo Fonte com Eslint e Prettier

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => (
+    <>
+        <View style={{ display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: '#c6c6c6', fontSize: 20 }}>Ola mundo</Text>
+        </View>
+    </>
+)
+
+export default App;
