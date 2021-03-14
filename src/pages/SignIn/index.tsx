198779ba2b91fefe1c5838bc4f57af1ff99a9f68
Fernando Maia <fermaiasoares@aol.com>
2021-03-14 11:13:21 -0300

    .

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
-import { KeyboardAvoidingView, StatusBar } from 'react-native';
+import { KeyboardAvoidingView, StatusBar, View, Platform } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/images/logo.png';
 
 import Input from '../../components/Input/index';
 import Button from '../../components/Button';
 
-import { Container, EsqueceuSenhaBotao, EsqueceuSenhaBotaoTexto, Image, TitlePage } from './styles';
+import { BotaoCriarConta, BotatoCriarContaTexto, Container, ContainerBody, EsqueceuSenhaBotao, EsqueceuSenhaBotaoTexto, Image, TitlePage } from './styles';
 
 const SignIn: React.FC = () => {
   useEffect(() => {
@@ -25,7 +26,7 @@ const SignIn: React.FC = () => {
         animated
       />
         <KeyboardAvoidingView
-          // behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
           style={{ flex: 1}}
           enabled
         >
@@ -39,41 +40,47 @@ const SignIn: React.FC = () => {
             }}
           >
             <Container>
-              <Image source={logoImg} />
-              <TitlePage style={{
-                elevation: 4,
-                shadowOffset: { width: 5, height: 5 },
-                shadowColor: '#321e38',
-                shadowOpacity: 0.5,
-                shadowRadius: 10,
-              }}
-              >Acesse sua conta</TitlePage>
+              <ContainerBody>
+                <Image source={logoImg} />
+                <TitlePage style={{
+                  elevation: 4,
+                  shadowOffset: { width: 5, height: 5 },
+                  shadowColor: '#321e38',
+                  shadowOpacity: 0.5,
+                  shadowRadius: 10,
+                }}
+                >Acesse sua conta</TitlePage>
 
-              <Input
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                autoCompleteType="email"
-                name="email"
-                label="E-mail"
-                icon="mail"
-                placeholder="Digite seu e-mail"
-              />
+                <Input
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  autoCompleteType="email"
+                  name="email"
+                  label="E-mail"
+                  icon="mail"
+                  placeholder="Digite seu e-mail"
+                />
 
-              <Input
-                keyboardType="default"
-                type="password"
-                name="password"
-                label="Senha"
-                icon="lock"
-                placeholder="Digite sua senha"
-              />
+                <Input
+                  keyboardType="default"
+                  type="password"
+                  name="password"
+                  label="Senha"
+                  icon="lock"
+                  placeholder="Digite sua senha"
+                />
 
-              <Button>Entrar</Button>
+                <Button onPress={() => console.log('Clicou em entrar')}>Entrar</Button>
 
-              <EsqueceuSenhaBotao>
-                <EsqueceuSenhaBotaoTexto>Esqueceu sua senha</EsqueceuSenhaBotaoTexto>
-              </EsqueceuSenhaBotao>
+                <EsqueceuSenhaBotao>
+                  <EsqueceuSenhaBotaoTexto>Esqueceu sua senha</EsqueceuSenhaBotaoTexto>
+                </EsqueceuSenhaBotao>
+              </ContainerBody>
+              <BotaoCriarConta>
+                <Icon name="log-in" size={20} color="#312e38" />
+                <BotatoCriarContaTexto>Criar uma Conta</BotatoCriarContaTexto>
+              </BotaoCriarConta>
             </Container>
           </LinearGradient>
       </KeyboardAvoidingView>
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
+  justify-content: space-between;
 `;
+
+export const ContainerBody = styled.View`
+  align-items: center;
+`
 
 export const ContainerFooter = styled.View`
   width: 100%;
@@ -30,3 +36,22 @@ export const EsqueceuSenhaBotaoTexto = styled.Text`
   font-size: 16px;
   color: #F5F5F5;
 `
+
+export const BotaoCriarConta = styled.View`
+  width: 340px;
+  height: 40px;
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 20px;
+  border-width: 1px;
+  border-color: #f5f5f5;
+
+  margin-bottom: ${ getBottomSpace() + 20 }px;
+`
+
+export const BotatoCriarContaTexto = styled.Text`
+`
+
